30c30f0c3f0c1b0789a556d8e6d709dd19f84658
derP@765pro <Giratina493@mew151.net>
2017-09-16 19:11:17 -0700

    CinderellaChannel class

--- spec/CinderellaChannel_spec.rb
+++ spec/CinderellaChannel_spec.rb
@@ -1,4 +1,44 @@
 require 'spec_helper'
 require_relative '../src/CinderellaChannel'
 describe CinderellaChannel do
+	before :each do
+		@channel = CinderellaChannel.new "test0", File.expand_path(File.dirname(__FILE__))
+		@channel2 = CinderellaChannel.new "test1", File.expand_path(File.dirname(__FILE__))
+		@failed_channel = CinderellaChannel.new "THIS ID DOESN'T EXIST", File.expand_path(File.dirname(__FILE__))
+	end
+
+	describe "#new" do
+		it "returns a CinderellaChannel object" do
+			expect(@channel).to be_an_instance_of CinderellaChannel
+		end
+		it "should fail gracefully" do
+			expect(@failed_channel).to be_an_instance_of CinderellaChannel
+			fileNotFound = CinderellaChannel.new "test0", "/brokenfilepath/"
+			expect(fileNotFound).to be_an_instance_of CinderellaChannel
+		end
+	end
+
+	describe "#identifier" do
+		it "should get the Channel's identifier" do
+			expect(@channel.identifier).to eq "test0"
+			expect(@channel2.identifier).to eq "test1"
+		end
+		it "should return nil on an invalid identifier" do
+			expect(@failed_channel.identifier).to eq nil
+		end
+	end
+
+	describe "#name" do
+		it "should get the Channel's name" do
+			expect(@channel.name).to eq "Test Radio 1"
+			expect(@channel2.name).to eq "Test Radio 52"
+		end
+	end
+
+	describe "#description" do
+		it "should get the Channel's description" do
+			expect(@channel.description).to eq "testy!"
+			expect(@channel2.description).to eq "heyo!"
+		end
+	end
 end
--- src/CinderellaChannel.rb
+++ src/CinderellaChannel.rb
@@ -1,3 +1,31 @@
+require "json"
 class CinderellaChannel
+	def initialize(identifier,config_location)
+		@identifier = identifier
+		@config_location = config_location
+		if File.file?(@config_location+"/Cinderella.json")
+			cinderella_data = JSON.parse(File.read(config_location+"/Cinderella.json"), object_class: OpenStruct)
+			channels = cinderella_data.channels.select do |channel|
+				channel.identifier == identifier
+			end
+			if channels.length > 0
+				@channel_data = channels[0]
+			else
+				@identifier = nil
+			end
+		else
+			@identifier = nil
+		end
+	end
 
+	def identifier
+		return @identifier
+	end
+
+	def name
+		return @channel_data.name
+	end
+	def description
+		return @channel_data.description
+	end
 end
